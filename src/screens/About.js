a4abe03374f6835c012d9d8a295d81c4add31a4c
John Lovato <jtlovato@gmail.com>
2021-05-28 14:16:27 -0600

    Updated about section

--- src/screens/About.js
+++ src/screens/About.js
@@ -3,15 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <h1>Meet The Team</h1>
-            <div className="team-member-container">
-                <div className="team-photo">
-                    Hi there
-                </div>
-                <div className="team-info">
-                    Hello
-                </div>
-            </div>
+
         </div>
     )
 }
